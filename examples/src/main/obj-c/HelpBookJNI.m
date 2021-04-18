0d02f53ab16d2dd4f8542f76c17485c309443368
Tobias Fischer <tobias.carl.fischer@gmail.com>
2021-04-18 13:22:09 +0200

    examples: fix typo in code comment

--- examples/src/main/obj-c/HelpBookJNI.m
+++ examples/src/main/obj-c/HelpBookJNI.m
@@ -1,10 +1,10 @@
 /*
-**    JNI hook for involking "NSApplication showHelp:"
+**    JNI hook for invoking "NSApplication showHelp:"
 **
 **   Notice that the function name is created using the string "Java_" and the package name
-**   using an underscore instead of a period, then the class name of bridge file. Finally the 
+**   using an underscore instead of a period, then the class name of bridge file. Finally the
 **   name of the 'public static native' method is appended.
-*/ 
+*/
 
 #import <JavaVM/jni.h>
 #import <Cocoa/Cocoa.h>
